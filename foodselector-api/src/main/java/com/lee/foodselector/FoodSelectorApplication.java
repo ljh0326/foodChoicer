2684ddb443d61d530671cc289d3d35c24ae77d74
Lee <ljh0326s@gmail.com>
2019-12-29 18:35:35 +0900

    make welcomePage

--- foodselector-api/src/main/java/com/lee/foodselector/FoodSelectorApplication.java
+++ foodselector-api/src/main/java/com/lee/foodselector/FoodSelectorApplication.java
@@ -1,13 +1,14 @@
 package com.lee.foodselector;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FoodselectorApplication {
+public class FoodSelectorApplication {
 
   public static void main(String[] args) {
-    SpringApplication.run(FoodselectorApplication.class, args);
+    SpringApplication.run(FoodSelectorApplication.class, args);
   }
 
 }
