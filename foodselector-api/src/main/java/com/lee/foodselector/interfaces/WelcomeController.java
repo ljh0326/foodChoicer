39d629cc7d480f46d1e2e89c782e22cdc2668c3c
lims2733@naver.com <ks157680>
2019-12-30 14:04:17 +0900

    메인 컨트롤러 구현 뷰단 vanila js로 비동기 구현(임시)

--- foodselector-api/src/main/java/com/lee/foodselector/interfaces/WelcomeController.java
+++ foodselector-api/src/main/java/com/lee/foodselector/interfaces/WelcomeController.java
@@ -1,13 +1,32 @@
 package com.lee.foodselector.interfaces;
 
+import lombok.extern.java.Log;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+
 @RestController
+@Log
 public class WelcomeController {
 
   @GetMapping("/")
   public  String hello(){
     return "Hello, world";
+  }
+
+
+  @GetMapping("/index")
+  public ResponseEntity<String> index()  {
+    try{
+
+      return new ResponseEntity<>("안녕하세요~~!",HttpStatus.OK);
+    }catch (Exception e){
+      e.printStackTrace();
+      return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+    }
   }
 }
