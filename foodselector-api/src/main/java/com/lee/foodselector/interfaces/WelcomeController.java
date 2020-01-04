9b36263fd77f42b3ba2f03e7686cc35ee1e3132e
Lee <ljh0326s@gmail.com>
2020-01-04 23:13:56 +0900

    자동배포 테스트

--- foodselector-api/src/main/java/com/lee/foodselector/interfaces/WelcomeController.java
+++ foodselector-api/src/main/java/com/lee/foodselector/interfaces/WelcomeController.java
@@ -1,13 +1,8 @@
 package com.lee.foodselector.interfaces;
 
 import lombok.extern.java.Log;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.servlet.http.HttpServletRequest;
 
 @RestController
 @Log
@@ -15,9 +10,6 @@ public class WelcomeController {
 
   @GetMapping("/")
   public  String hello(){
-    return "Hello, world";
+    return "Hello, 자동배포";
   }
-
-
-
 }
